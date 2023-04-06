1c0da747af224180a173d40abc07fb0851ca14f7
Shehan Rangana <shehanr@swivelgroup.com.au>
2023-04-06 09:47:14 +0530

    Database connection configurations.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { EmployeesModule } from './api/employees/employees.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URL),
+    EmployeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
